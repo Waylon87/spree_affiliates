65e4a6f12e655e47ab10c75de44326628335dffe
Waylon Peele <waylon@cloudcliques.com>
2015-01-26 11:21:34 -0800

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Spree::Core::Engine.routes.draw do
   match 'admin/affiliate_settings/show' =>  'admin/affiliate_settings#show', :as => :admin_affiliate_settings, via: :get
-  match 'admin/affiliate_settings/edit' =>  'admin/affiliate_settings#edit', :as => :admin_edit_affiliate_settings, via: [:get, :post]
-  match 'admin/affiliate_settings/update' =>  'admin/affiliate_settings#update', :as => :admin_update_affiliate_settings, via: [:get, :post]
-  match 'admin/affiliate_credits/update' =>  'admin/affiliate_credits#update', :as => :admin_update_affiliate_credits, via: [:get, :post]
+  match 'admin/affiliate_settings/edit' =>  'admin/affiliate_settings#edit', :as => :admin_edit_affiliate_settings, via: :get
+  match 'admin/affiliate_settings/update' =>  'admin/affiliate_settings#update', :as => :admin_update_affiliate_settings, via: :post
+  match 'admin/affiliate_credits/update' =>  'admin/affiliate_credits#update', :as => :admin_update_affiliate_credits, via: :post
   match 'affiliates/whats_this' =>  'affiliates#whats_this', :as => :whats_this, via: :get
   match 'affiliates/show_details' =>  'affiliates#show_details', :as => :show_details, via: :get
-  match 'affiliates/request_payment' =>  'affiliates#request_payment', :as => :request_payment, via: [:get, :post]
-  match 'affiliates/pay_user' =>  'affiliates#pay_user', :as => :pay_user, via: [:get, :post]
+  match 'affiliates/request_payment' =>  'affiliates#request_payment', :as => :request_payment, via: :post
+  match 'affiliates/pay_user' =>  'affiliates#pay_user', :as => :pay_user, via: :post
   namespace :admin do
     resources :user do
       resources :affiliate_credits
