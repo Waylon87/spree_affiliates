fb43a9ff674bc90931aa8e922c2830cb5314c254
Waylon Peele <waylon@cloudcliques.com>
2015-01-26 14:24:10 -0800

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Spree::Core::Engine.routes.draw do
   match 'admin/affiliate_settings/show' =>  'admin/affiliate_settings#show', :as => :admin_affiliate_settings, via: :get
   match 'admin/affiliate_settings/edit' =>  'admin/affiliate_settings#edit', :as => :admin_edit_affiliate_settings, via: :get
   match 'admin/affiliate_settings/update' =>  'admin/affiliate_settings#update', :as => :admin_update_affiliate_settings, via: [:patch, :put]
-  match 'admin/affiliate_credits/update' =>  'admin/affiliate_credits#update', :as => :admin_update_affiliate_credits, via: [:patch, :put]
+  match 'admin/affiliate_credits/update' =>  'admin/affiliate_credits#update', :as => :admin_update_affiliate_credits, via: :post
   match 'affiliates/whats_this' =>  'affiliates#whats_this', :as => :whats_this, via: :get
   match 'affiliates/show_details' =>  'affiliates#show_details', :as => :show_details, via: :get
   match 'affiliates/request_payment' =>  'affiliates#request_payment', :as => :request_payment, via: :get
-  match 'affiliates/pay_user' =>  'affiliates#pay_user', :as => :pay_user, via: :post
+  match 'affiliates/pay_user' =>  'affiliates#pay_user', :as => :pay_user, via: :get
   namespace :admin do
     resources :users do
       resources :affiliate_credits
